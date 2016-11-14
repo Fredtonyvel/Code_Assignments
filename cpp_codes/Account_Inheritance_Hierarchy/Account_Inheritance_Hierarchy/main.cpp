2a2e35e82b77f3334f5ecb842683f361755e6078
Fredtonyvel <vfredanthony@yahoo.com>
2016-11-14 14:40:10 -0500

    Added a working menu to main.cpp

--- cpp_codes/Account_Inheritance_Hierarchy/Account_Inheritance_Hierarchy/main.cpp
+++ cpp_codes/Account_Inheritance_Hierarchy/Account_Inheritance_Hierarchy/main.cpp
@@ -15,57 +15,59 @@ int main()
     Account obj1(balance);
 
     char select;
+    cout << "\nWould you to start your transaction?\n";
+    cout << "Enter 'y' for yes\nEnter 'n' for no\n";
     cin >> select;
 
     do
     {
-        cout << "\nChoose from the following:\n";
-        cout << "1. Account balance\n";
-        cout << "2. Credit\n";
-        cout << "3. Debit\n";
-        cout << "4. Set balance\n";
-        cout << "5. Print balance\n\n";
-
-        int choice;
-        cin >> choice;
-
-        switch(choice)
+        if (select == 'y')
         {
-            /*case 1: int balance;
-                      cout << "Enter balance for your account\n";
-                      cin >> balance;
-                      Account obj1(balance);
-                      break;*/
+            cout << "\nChoose from the following:\n";
+            cout << "1. Credit\n";
+            cout << "2. Debit\n";
+            cout << "3. Set balance\n";
+            cout << "4. Print balance\n";
+            cout << "5. Exit\n\n";
 
-            case 2: int credit;
-                    cout << "Enter the amount of credit\n";
-                    cin >> credit;
-                    obj1.credit(credit);
-                    break;
+            int choice;
+            cin >> choice;
 
-            case 3: int debit;
-                    cout << "Enter the amount of debit\n";
-                    cin >> debit;
-                    obj1.debit(debit);
-                    break;
+            switch(choice)
+            {
+                case 1: int credit;
+                        cout << "Enter the amount of credit\n";
+                        cin >> credit;
+                        obj1.credit(credit);
+                        break;
 
-            case 4: int setBalance;
-                    cout << "Enter amount to reset yout balance\n";
-                    cin >> setBalance;
-                    obj1.setBalance(setBalance);
-                    break;
+                case 2: int debit;
+                        cout << "Enter the amount of debit\n";
+                        cin >> debit;
+                        obj1.debit(debit);
+                        break;
 
-            case 5: cout << fixed << setprecision(2);
-                    cout << "---\tCurrent balance in your account = "
-                         << obj1.getBalance() << "\t---" << endl;
-                    break;
+                case 3: int setBalance;
+                        cout << "Enter amount to reset yout balance\n";
+                        cin >> setBalance;
+                        obj1.setBalance(setBalance);
+                        break;
 
-            default: cout << "!!!\tPlease select a number\t!!!\n";
-        }
+                case 4: cout << fixed << setprecision(2);
+                        cout << "---\tCurrent balance in your account = "
+                             << obj1.getBalance() << "\t---" << endl;
+                        break;
 
-        cout << "\nPress 'y' to continue\n";
-        cin >> select;
-    } while (select == 'y');
+                case 5: select = 'n';
+                        break;               
+
+                default: cout << "!!!\tPlease select a number\t!!!\n";
+            }
+        }   
+
+    } while (select != 'n' && select == 'y');
+
+    cout << "\n\nExiting System\t---\tHave a nice day!\n\n";
     
 
     /*Account obj(2.50);
