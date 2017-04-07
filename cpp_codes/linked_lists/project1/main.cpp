779959334ca49c32e38964249eaf96982a74c60f
Fredtonyvel <vfredanthony@yahoo.com>
2017-04-06 23:07:38 -0400

    Edited main.cpp

--- cpp_codes/linked_lists/project1/main.cpp
+++ cpp_codes/linked_lists/project1/main.cpp
@@ -8,10 +8,10 @@ using namespace std;
 
 int main()
 {
-	Professor s("Anthony", "Velez", "5.15NB", "vfredanthony@yahoo.com", "347-813-6946");
-	Professor m("Angelica", "Velez", "6.63NB", "a_vele@hotmail.com", "347-239-1576");
-	Professor d("Emely", "Velez", "7.63NB", "emely_vele@gmail.com", "347-239-4675");
-	Professor f("Freddy", "Velez", "10.63NB", "f_velez@gmail.com", "347-528-0598");
+	Professor s("Tony", "Doe", "5.15NB", "tonydoe@aol.com", "646-555-2697");
+	Professor m("Jane", "Dane", "6.63NB", "janedane@aol.com", "917-654-7891");
+	Professor d("Karen", "Page", "7.63NB", "karenpage@aol.com", "347-239-4675");
+	Professor f("Bruce", "Allen", "10.63NB", "bruceallen@aol.com", "718-693-3348");
 
 	list ll;
 	ll.addNode(s,23);
