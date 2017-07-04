2a266cb3b08b85a472caf0912ad5e2f63259910d
Fredtonyvel <vfredanthony@yahoo.com>
2017-07-03 23:44:50 -0400

    Used sqlite API to create a table and write queries.

--- cpp_codes/linked_lists/project1/main.cpp
+++ cpp_codes/linked_lists/project1/main.cpp
@@ -15,16 +15,9 @@ using namespace std;
 
 static int callback(void *NotUsed, int argc, char **argv, char **azColName)
 {
-	for(int i = 0; i<argc; i++)
-    {
-		cout << left << setw(20) << azColName[i] << setw(20);
-	}
-	cout << endl;
-
     for(int i = 0; i<argc; i++)
     {
-       //printf("%s = %s\n", azColName[i], argv[i] ? argv[i] : "NULL");
-   		cout << left << setw(20) << argv[i] << setw(20);
+        printf("%s = %s\n", azColName[i], argv[i] ? argv[i] : "NULL");
     }
     cout << endl;
 
@@ -33,13 +26,13 @@ static int callback(void *NotUsed, int argc, char **argv, char **azColName)
 
 int main(int argc, char* argv[])
 {
-	/*Professor s("Tony", "Doe", "5.15NB", "tonydoe@aol.com", "646-555-2697");
+	Professor s("Tony", "Doe", "5.63NB", "tonydoe@aol.com", "646-555-2697");
 	Professor m("Jane", "Dane", "6.63NB", "janedane@aol.com", "917-654-7891");
 	Professor d("Karen", "Page", "7.63NB", "karenpage@aol.com", "347-239-4675");
 	Professor f("Bruce", "Allen", "10.63NB", "bruceallen@aol.com", "718-693-3348");
-	Professor g("Freddy", "Velez", "6.63NB", "freddyvelez@aol.com", "347-754-6251");
-	Professor h("Anthony", "Velez", "6.63NB", "anthonyvelez@aol.com", "347-154-7251");
-	Professor i("Kenneth", "Cole", "10.63NB", "bruceallen@aol.com", "718-693-3348");
+	Professor g("Freddy", "Velez", "9.63NB", "freddyvelez@aol.com", "347-754-6251");
+	Professor h("Anthony", "Velez", "4.63NB", "anthonyvelez@aol.com", "347-154-7251");
+	Professor i("Kenneth", "Cole", "8.63NB", "kennethcole@aol.com", "718-583-2236");
 
 	list ll;
 	ll.insert(s);
@@ -49,8 +42,9 @@ int main(int argc, char* argv[])
 	ll.insert(g);
 	ll.insert(h);
 	ll.insert(i);
-	ll.display();
-	ll.save();*/
+	//ll.display();
+	ll.save();
+	ll.load();
 	//cout << "\nSize of professor list: " << ll.size() << endl;
 
 	/*string fname, lname;
@@ -61,7 +55,7 @@ int main(int argc, char* argv[])
 	cin >> lname;
 	cout << ll.search(fname, lname) << endl;*/
 
-	sqlite3 *db;
+	/*sqlite3 *db;
 	char *zErrMsg = 0;
 	int rc;
 
@@ -83,7 +77,7 @@ int main(int argc, char* argv[])
 		  "LAST_NAME			TEXT		NOT NULL,"	\
 		  "FIRST_NAME			TEXT		NOT NULL,"	\
 		  "OFFICE_NO			TEXT		NOT NULL,"	\
-		  "EMAIL				CHAR(70),"	\
+		  "EMAIL 				CHAR(70),"	\
 		  "PHONE_NO				CHAR(15));";
 
 	//Executing SQL statement
@@ -97,34 +91,33 @@ int main(int argc, char* argv[])
 	else
 		cout << "Table created successfully" << endl;
 
-	Professor profArray[7];
 
 	//Inserting values into Table
-	const char * ln;
-	const char * fn;
-	const char * rn;
-	const char * em;
-	const char * ph;
-	string sql2[7];
+
+	/*Professor * profArray[7];
+	profArray[0] = new Professor("Tony", "Doe", "5.63NB", "tonydoe@aol.com", "646-555-2697");
+	profArray[1] = new Professor("Jane", "Dane", "6.63NB", "janedane@aol.com", "917-654-7891");
+	profArray[2] = new Professor("Karen", "Page", "7.63NB", "karenpage@aol.com", "347-239-4675");
+	profArray[3] = new Professor("Bruce", "Allen", "10.63NB", "bruceallen@aol.com", "718-693-3348");
+	profArray[4] = new Professor("Freddy", "Velez", "9.63NB", "freddyvelez@aol.com", "347-754-6251");
+	profArray[5] = new Professor("Anthony", "Velez", "4.63NB", "anthonyvelez@aol.com", "347-154-7251");
+	profArray[6] = new Professor("Kenneth", "Cole", "8.63NB", "kennethcole@aol.com", "718-583-2236");*/
+	
+	/*string sql2[7];
 	const char *sql2Stmt;
+
 
 	for(int i = 0; i < 7; i++)
 	{	
-		cin >> profArray[i];
-
-		/*ln = profArray[i].getLastName().c_str();
-		fn = profArray[i].getFirstName().c_str();
-		rn = profArray[i].getRoomNum().c_str();
-		em = profArray[i].getEmail().c_str();
-		ph = profArray[i].getPhone().c_str();*/
+		//cin >> profArray[i];
 
 		sql2[i] = "INSERT INTO JJAY(ID, LAST_NAME, FIRST_NAME, OFFICE_NO, EMAIL, PHONE_NO) " \
 		                   "VALUES (" + to_string(i+1) + ", '" 
-		               	             + profArray[i].getLastName().c_str() + "', '"
-		               	             + profArray[i].getFirstName().c_str() + "', '"
-		               	             + profArray[i].getRoomNum().c_str() + "', '"
-		               	             + profArray[i].getEmail().c_str() + "', '"
-		               	             + profArray[i].getPhone().c_str() + "');";
+		               	             + profArray[i]->getLastName().c_str() + "', '"
+		               	             + profArray[i]->getFirstName().c_str() + "', '"
+		               	             + profArray[i]->getRoomNum().c_str() + "', '"
+		               	             + profArray[i]->getEmail().c_str() + "', '"
+		               	             + profArray[i]->getPhone().c_str() + "');";
 		sql2Stmt = sql2[i].c_str();
 	
 		rc = sqlite3_exec(db, sql2Stmt, 0, 0, &zErrMsg);
@@ -150,7 +143,41 @@ int main(int argc, char* argv[])
 	else
 		cout << "Operation done successfully" << endl;
 
-	sqlite3_close(db);
+	sqlite3_close(db);*/
+
+	/*sqlite3 *db;
+	char *zErrMsg = 0;
+	int rc;
+
+	//Opening database
+	rc = sqlite3_open("test.db", &db);
+
+	if(rc)
+	{
+		cout << "Can't open database" << sqlite3_errmsg(db) << endl;
+		return 0;
+	}	
+	else
+		cout << "Opened database successfully" << endl;
+
+	//Create SQL statement
+	string sql;
+	while(sql != "quit")
+	{
+		getline(cin,sql);
+
+		//Executing SQL statement
+		rc = sqlite3_exec(db, sql.c_str(), callback, 0, &zErrMsg);
+		if(rc != SQLITE_OK)
+		{
+			cout << "SQL error: " << zErrMsg << endl;
+		 	sqlite3_free(zErrMsg);
+		}
+		else
+			cout << "Operation done successfully" << endl;
+	}
+
+	sqlite3_close(db);*/
 
 	return 0;
 }
